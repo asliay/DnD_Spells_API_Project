05d06c3f7948b556dce65bf9e53dcf5486cba573
Ailsa Young <ailsa.young16@hotmail.co.uk>
2021-01-02 18:12:50 +0000

    button now randomising character race and class name

--- src/containers/CharacterBox.js
+++ src/containers/CharacterBox.js
@@ -7,23 +7,46 @@ const CharacterBox = () => {
   
   const [races, setRaces] = useState([]);
   const [randomRace, setRandomRace] = useState('');
+  const [classes, setClasses] = useState([]);
+  const [randomClass, setRandomClass] = useState('');
 
   const fetchRaces = () => {
-    const url = `https://www.dnd5eapi.co/api/races`;
-    fetch(url)
+    const racesUrl = `https://www.dnd5eapi.co/api/races`;
+    fetch(racesUrl)
       .then((response) => response.json())
       .then((data) => {
         console.log(data.results);
         setRaces(data.results);
       });
   }
+
+  const fetchClasses = () => {
+    const classesUrl = `https://www.dnd5eapi.co/api/classes`
+    fetch(classesUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.results);
+        setClasses(data.results);
+      });
+  }
+  const getRandomRace = () => {
+    const randomRace = races[Math.floor(Math.random() * races.length)];
+    setRandomRace(randomRace.name); //value assigned here
+  }
+
+  const getRandomClass = () => {
+    const randomClass = classes[Math.floor(Math.random() * classes.length)];
+    setRandomClass(randomClass.name);
+  }
+
   const handleClick = () => {
-    const random = races[Math.floor(Math.random() * races.length)];
-    setRandomRace(random.name);//value assigned here
+    getRandomRace();
+    getRandomClass();
   };
 
   useEffect(() => {
     fetchRaces();
+    fetchClasses();
   }, [])
 
   return (
@@ -31,6 +54,7 @@ const CharacterBox = () => {
     <h1>Randomise a D&D Character!</h1>
     <button onClick={handleClick}>Click me</button>
     <p>{randomRace}</p>
+    <p>{randomClass}</p>
     
     </>
   )
