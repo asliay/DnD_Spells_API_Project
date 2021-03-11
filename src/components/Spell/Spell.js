e88b5d5b4739076a1b83940a035478667c805d78
Ailsa Young <ailsa.young16@hotmail.co.uk>
2021-03-11 20:34:40 +0000

    Removed conditional render functions for spell info  and used ternary operator instead

--- src/components/Spell/Spell.js
+++ src/components/Spell/Spell.js
@@ -2,32 +2,23 @@ import "./Spell.css"
 
 const Spell = ({spellInfo}) => {
 
-  let spellLevel;
-  // checks spell level in order to render accurately (cantrips are technically spell level 0 but are referred to as cantrips)
-  if (spellInfo.level > 0) {
-    spellLevel = `Level ${spellInfo.level} ${spellInfo.school['name']}`
-  }
-  else {
-    spellLevel = `${spellInfo.school['name']} Cantrip`
-  };
-
-  let spellMaterials;
-  // only renders spell material if it exists
-  if (spellInfo.material) spellMaterials = ` (${spellInfo.material})`
-
-  let spellRitual;
-  // checks whether ritual === true before rendering
-  if (spellInfo.ritual) spellRitual = ` (ritual)`
+  
+  const spellClasses = spellInfo.classes.map((classes => (classes.name))).join(", ")
 
   return (
     <>
     <h2 id="spell-name">{spellInfo.name}</h2>
     <div id="spell-info">
-      <p id="spell-level">{spellLevel} {spellRitual}</p>
+      <p id="spell-level">
+        {spellInfo.level > 0 ? `Level ${spellInfo.level} ${spellInfo.school['name']}` : `${spellInfo.school['name']} Cantrip`} {spellInfo.ritual ? ` (ritual)` : null}
+        </p>
       <p id="spell-casting-time">Casting Time: {spellInfo.casting_time}</p>
+      <p id="spell-classes">Class: {spellClasses}</p>
       <p id="spell-range">Range: {spellInfo.range}</p>
       {/* .join() allows components array to be rendered seperated by comma */}
-      <p id="spell-components">Components: {spellInfo.components.join(", ")}{spellMaterials}</p>
+      <p id="spell-components">
+        Components: {spellInfo.components.join(", ")} {spellInfo.material ? `(${spellInfo.material.slice(0, -1)})` : null}
+      </p>
       <p id="spell-duration">Duration: {spellInfo.duration}</p>
     </div>
       <p id="spell-desc">{spellInfo.desc}</p>
